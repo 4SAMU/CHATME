893a365602060cbb769c4bdb18f615d6e3391945
4SAMU <samuelnzomo4@gmail.com>
2023-03-16 12:36:53 +0300

    Automatically committed changes on my Projects page

--- App.js
+++ App.js
@@ -1,23 +1,50 @@
-import React from "react";
-import { StyleSheet, Text, SafeAreaView } from "react-native";
+import React, {useState} from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableNativeFeedback,
+  StatusBar,
+} from 'react-native';
 
 const App = () => {
+  const [rippleColor, setRippleColor] = useState(randomHexColor());
+  const [rippleOverflow, setRippleOverflow] = useState(false);
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.text}>Page content</Text>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <TouchableNativeFeedback
+        onPress={() => {
+          setRippleColor(randomHexColor());
+          setRippleOverflow(!rippleOverflow);
+        }}
+        background={TouchableNativeFeedback.Ripple(
+          rippleColor,
+          rippleOverflow,
+        )}>
+        <View style={styles.touchable}>
+          <Text style={styles.text}>TouchableNativeFeedback</Text>
+        </View>
+      </TouchableNativeFeedback>
+    </View>
   );
+};
+
+const randomHexColor = () => {
+  return '#000000'.replace(/0/g, function () {
+    return Math.round(Math.random() * 16).toString(16);
+  });
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    paddingTop: StatusBar.currentHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
   },
-  text: {
-    fontSize: 25,
-    fontWeight: "500",
-    paddingLeft: "50%",
-  },
+  touchable: {flex: 0.5, borderColor: 'black', borderWidth: 1},
+  text: {alignSelf: 'center'},
 });
 
 export default App;
