87ac0c8eb7984781d1c4f202609b16a70b78a582
4SAMU <samuelnzomo4@gmail.com>
2023-03-16 17:52:11 +0300

    Automatically committed changes on my Projects page

--- components/dmchats/DirectMessage.js
+++ components/dmchats/DirectMessage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { ScrollView, View } from "react-native";
-import DmStyles from "./styles";
+import DmStyles from "./dmstyles";
 
 export const DirectMessage = () => {
   return (
