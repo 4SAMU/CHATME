c6da137794856c3c64ad132b4a4de3ee067d54b6
4SAMU <samuelnzomo4@gmail.com>
2023-03-16 13:24:47 +0300

    Automatically committed changes on my Projects page

--- App.js
+++ App.js
@@ -30,7 +30,7 @@ const DATA = [
 
 export default App = () => (
   <SafeAreaView style={styles.container}>
-    <Button
+    <Button></Button>
     <SectionList
       sections={DATA}
       keyExtractor={(item, index) => item + index}
