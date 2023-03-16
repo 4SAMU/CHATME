bbd803d2b545b2f7b7cd1fd0b23c88b2ea9137e3
4SAMU <samuelnzomo4@gmail.com>
2023-03-16 18:37:20 +0300

    Automatically committed changes on my Projects page

--- App.js
+++ App.js
@@ -17,7 +17,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "black",
-    position: "absol",
+    position: "absolute",
   },
   content: {
     zIndex: 0,
