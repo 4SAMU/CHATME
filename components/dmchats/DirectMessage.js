2afec53aeffed153a2d26650a6d7678110bca595
4SAMU <samuelnzomo4@gmail.com>
2023-03-16 17:55:56 +0300

    Automatically committed changes on my Projects page

--- components/dmchats/DirectMessage.js
+++ components/dmchats/DirectMessage.js
@@ -7,9 +7,7 @@ export const DirectMessage = () => {
     <View>
       <View style={dmStyles.container}>
         <View style={dmStyles.box}>mama</View>
-        <View style={dmStyles.box}>baba</View>
-        <View style={dmStyles.box}>mtoto</View>
-        <View style={dmStyles.box}>mjukuu</View>
+        
       </View>
     </View>
   );
