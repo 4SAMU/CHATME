53c6cbce27fb9191e7a094d94dc41bd60902b24c
4SAMU <samuelnzomo4@gmail.com>
2023-03-16 13:31:30 +0300

    Automatically committed changes on my Projects page

--- App.js
+++ App.js
@@ -1,16 +1,1 @@
-import React from "react";
-import { StyleSheet, Text, SafeAreaView, StatusBar, Pressable, Alert } from "react-native";
-import { Naame } from "./zip";
-
-export default App = () => (
- 
-
-
-
-  <SafeAreaView style={styles.container}>
-    <Pressable onPress={name}>
-      <Text>I'm pressable!</Text>
-    </Pressable>
-  </SafeAreaView>
-);
-
+rafc
