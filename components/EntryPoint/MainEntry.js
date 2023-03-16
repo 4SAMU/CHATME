0ef5dba985143094d74d7bf61e8d1486e039c13b
4SAMU <samuelnzomo4@gmail.com>
2023-03-16 19:28:04 +0300

    Automatically committed changes on my Projects page

--- components/EntryPoint/MainEntry.js
+++ components/EntryPoint/MainEntry.js
@@ -12,7 +12,7 @@ import { DirectMessage } from "../dmchats/DirectMessage";
 const { width } = Dimensions.get("window");
 const { height } = Dimensions.get("window");
 const DmsHeight = height - 50;
-console.log("height", height - 50);
+console.log("height", );
 
 
 export const MainEntry = () => {
