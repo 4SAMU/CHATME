6e9db16e6785ca2db98632a0437170698e7d5d24
4SAMU <samuelnzomo4@gmail.com>
2023-03-16 12:27:48 +0300

    Automatically committed changes on my Projects page

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "",
+    backgroundColor: "yellow",
     alignItems: "center",
     justifyContent: "center",
     paddingTop: StatusBar.currentHeight,
