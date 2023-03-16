b05eea526ba06ec5aea734f78897e928551966df
4SAMU <samuelnzomo4@gmail.com>
2023-03-16 17:11:11 +0300

    Automatically committed changes on my Projects page

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -24,7 +24,7 @@ export const Navbar = () => {
       drawerPosition="right"
       renderNavigationView={() => (
         <View style={navStyles.drawer}>
-          <Text>Drawer content</Text>
+          <Text></Text>
           <Text>Drawer content</Text>
           <Text>Drawer content</Text>
         </View>
