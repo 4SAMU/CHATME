65881f18f2af05a2b4240875597b1ac20fac2fa2
4SAMU <samuelnzomo4@gmail.com>
2023-03-16 14:31:41 +0300

    Automatically committed changes on my Projects page

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -5,7 +5,7 @@ import navStyles from "./styles";
 export const Navbar = () => {
   return (
     <View styles={navStyles.navbar}>
-      <Text styles={navStyles.text}></Text>
+      <Text styles={navStyles.text}>hello</Text>
     </View>
   );
 };
