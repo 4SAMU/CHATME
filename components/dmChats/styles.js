dd737e927331cdfa3d042fe19825ae9542c6964b
4SAMU <samuelnzomo4@gmail.com>
2023-03-16 17:37:04 +0300

    Automatically committed changes on my Projects page

---
+++ components/dmChats/styles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+const DmStyles = StyleSheet.create({
+  box: {
+    height: 60,
+    backgroundColor: "#030C1A",
+  },
+});
+
+export default DmStyles;
