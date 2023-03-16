ec55f2d18ac167329115b3cc55d1367d2bcdbd6b
4SAMU <samuelnzomo4@gmail.com>
2023-03-16 13:11:41 +0300

    Automatically committed changes on my Projects page

--- zip.js
+++ zip.js
@@ -1,4 +1,4 @@
 function name(params) {
    let A = [{A:"A"}, {B:"B"}, {C:"c"}]
-   let B = [[{a:}, "b", "c"], ["d", "e", "f"], ["g", "h", "i"]]  
+   let B = [[{a:""}, "b", "c"], ["d", "e", "f"], ["g", "h", "i"]]  
 }
