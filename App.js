5c608b3b6ac42ca216014dc729ba0c2911d0394a
4SAMU <samuelnzomo4@gmail.com>
2023-03-16 17:50:18 +0300

    Automatically committed changes on my Projects page

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { SafeAreaView, StatusBar, StyleSheet, View } from "react-native";
 import { DirectMessage } from "./components/dmchats/DirectMessage";
-import { Navbar } from "./components/navbar/navbar";
 
 const App = () => {
   return (
