5f6a206c1e980f540b11327c4e9b0d24eb254eb9
4SAMU <samuelnzomo4@gmail.com>
2023-03-16 18:13:16 +0300

    Automatically committed changes on my Projects page

--- App.js
+++ App.js
@@ -22,7 +22,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "black",
   },
-  mydata:{
-    backgroundColor:'red'
-  }
+  mydata: {
+    backgroundColor: "red",
+  },
 });
