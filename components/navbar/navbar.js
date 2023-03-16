a2cc3a73e4ba6f68f602abfddc5259f0b4caa219
4SAMU <samuelnzomo4@gmail.com>
2023-03-16 18:51:22 +0300

    Automatically committed changes on my Projects page

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -8,6 +8,7 @@ import {
 } from "react-native";
 import navStyles from "./styles";
 import Icon from "react-native-vector-icons/Feather";
+import { DirectMessage } from "../dmchats/DirectMessage";
 const { width } = Dimensions.get("window");
 const { height } = Dimensions.get("window");
 
@@ -51,7 +52,7 @@ export const Navbar = () => {
           </View>
         </TouchableOpacity>
       </View>
-      <D
+      <DirectMessage/>
     </DrawerLayoutAndroid>
   );
 };
