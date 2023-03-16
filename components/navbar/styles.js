6da31c927f9626900d9b1acc3e9ec44b1d4d0e0c
4SAMU <samuelnzomo4@gmail.com>
2023-03-16 14:11:08 +0300

    Automatically committed changes on my Projects page

---
+++ components/navbar/styles.js
@@ -0,0 +1,13 @@
+const styles = StyleSheet.create({
+    navbar: {
+      height: 100,
+      backgroundColor: 'blue',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    title: {
+      color: 'white',
+      fontSize: 20,
+      fontWeight: 'bold',
+    },
+  });
