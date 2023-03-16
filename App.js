868f24b85cb0a0ed43d9194a801508099949e097
4SAMU <samuelnzomo4@gmail.com>
2023-03-16 14:26:27 +0300

    Automatically committed changes on my Projects page

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "black",
-    paddingTop:StatusBar.currentHeight
+    paddingTop: StatusBar.currentHeight,
   },
 });
 
