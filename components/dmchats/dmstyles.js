eb36138d61792c5daea2eed6b8074359153ba36b
4SAMU <samuelnzomo4@gmail.com>
2023-03-16 18:51:52 +0300

    Automatically committed changes on my Projects page

--- components/dmchats/dmstyles.js
+++ components/dmchats/dmstyles.js
@@ -2,7 +2,7 @@ import { StyleSheet } from "react-native";
 
 const dmStyles = StyleSheet.create({
   container: {
-    flex:1,
+    flex: 1,
     backgroundColor: "#030C1A",
     paddingTop: 60,
   },
