0a297377694f54fe36d41ccde6626d1cecf28f94
4SAMU <samuelnzomo4@gmail.com>
2023-03-16 19:59:35 +0300

    Automatically committed changes on my Projects page

--- components/dmchats/DirectMessage.js
+++ components/dmchats/DirectMessage.js
@@ -6,50 +6,51 @@ const DirectMessage = () => {
   return (
     <View style={dmStyles.container}>
       <ScrollView>
-      <View style={dmStyles.items}>
-        <View style={dmStyles.box}>
-          <Text style={dmStyles.text}>Mama</Text>
+        <View style={dmStyles.items}>
+          <View style={dmStyles.box}>
+            <Text style={dmStyles.text}>Mama</Text>
+          </View>
+          <View style={dmStyles.box}>
+            <Text style={dmStyles.text}>Mama</Text>
+          </View>
+          <View style={dmStyles.box}>
+            <Text style={dmStyles.text}>Mama</Text>
+          </View>
+          <View style={dmStyles.box}>
+            <Text style={dmStyles.text}>Mama</Text>
+          </View>
+          <View style={dmStyles.box}>
+            <Text style={dmStyles.text}>Mama</Text>
+          </View>
+          <View style={dmStyles.box}>
+            <Text style={dmStyles.text}>Mama</Text>
+          </View>
+          <View style={dmStyles.box}>
+            <Text style={dmStyles.text}>Mama</Text>
+          </View>
+          <View style={dmStyles.box}>
+            <Text style={dmStyles.text}>Mama</Text>
+          </View>
+          <View style={dmStyles.box}>
+            <Text style={dmStyles.text}>Mama</Text>
+          </View>
+          <View style={dmStyles.box}>
+            <Text style={dmStyles.text}>Mama</Text>
+          </View>
+          <View style={dmStyles.box}>
+            <Text style={dmStyles.text}>Mama</Text>
+          </View>
+          <View style={dmStyles.box}>
+            <Text style={dmStyles.text}>Mama</Text>
+          </View>
+          <View style={dmStyles.box}>
+            <Text style={dmStyles.text}>Mama</Text>
+          </View>
+          <View style={dmStyles.box}>
+            <Text style={dmStyles.text}>Mama</Text>
+          </View>
         </View>
-        <View style={dmStyles.box}>
-          <Text style={dmStyles.text}>Mama</Text>
-        </View>
-        <View style={dmStyles.box}>
-          <Text style={dmStyles.text}>Mama</Text>
-        </View>
-        <View style={dmStyles.box}>
-          <Text style={dmStyles.text}>Mama</Text>
-        </View>
-        <View style={dmStyles.box}>
-          <Text style={dmStyles.text}>Mama</Text>
-        </View>
-        <View style={dmStyles.box}>
-          <Text style={dmStyles.text}>Mama</Text>
-        </View>
-        <View style={dmStyles.box}>
-          <Text style={dmStyles.text}>Mama</Text>
-        </View>
-        <View style={dmStyles.box}>
-          <Text style={dmStyles.text}>Mama</Text>
-        </View>
-        <View style={dmStyles.box}>
-          <Text style={dmStyles.text}>Mama</Text>
-        </View>
-        <View style={dmStyles.box}>
-          <Text style={dmStyles.text}>Mama</Text>
-        </View>
-        <View style={dmStyles.box}>
-          <Text style={dmStyles.text}>Mama</Text>
-        </View>
-        <View style={dmStyles.box}>
-          <Text style={dmStyles.text}>Mama</Text>
-        </View>
-        <View style={dmStyles.box}>
-          <Text style={dmStyles.text}>Mama</Text>
-        </View>
-        <View style={dmStyles.box}>
-          <Text style={dmStyles.text}>Mama</Text>
-        </View>
-      </View>
+      </ScrollView>
     </View>
   );
 };
