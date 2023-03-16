284533c569e4b74bc5cb7a3a46ae3cfdd82b3e97
4SAMU <samuelnzomo4@gmail.com>
2023-03-16 13:32:36 +0300

    Automatically committed changes on my Projects page

--- App.js
+++ App.js
@@ -2,11 +2,14 @@ import React from 'react'
 import { SafeAreaView } from 'react-native/types'
 
 const App = () => {
-
+  function name() {
+    let x=Naame();
+    Alert.alert(x)
+   }
 
   return (
     <SafeAreaView>
-      
+
     </SafeAreaView>
   )
 }
