d4f220539962e546d8ee882b6d753c112af2107c
4SAMU <samuelnzomo4@gmail.com>
2023-03-16 12:30:40 +0300

    Automatically committed changes on my Projects page

--- App.js
+++ App.js
@@ -18,16 +18,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    paddingTop: StatusBar.currentHeight,
   },
-  Text: {
-    color: "#fff",
+  image: {
+    flex: 1,
+    justifyContent: 'center',
   },
-  nav: {
-    position: "absolute",
-    width: "100%",
-    backgroundColor: "yellow",
+  text: {
+    color: 'white',
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: '#000000c0',
   },
 });
