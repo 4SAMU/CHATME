705700c77be1bc8cafbbf70645831cee5412e3d8
4SAMU <samuelnzomo4@gmail.com>
2023-03-16 12:51:19 +0300

    Automatically committed changes on my Projects page

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {ActivityIndicator, StyleSheet, View} from 'react-native';
+import React from "react";
+import { ActivityIndicator, StyleSheet, View } from "react-native";
 
 const App = () => (
   <View style={[styles.container, styles.horizontal]}>
@@ -13,11 +13,11 @@ const App = () => (
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: "center",
   },
   horizontal: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
+    flexDirection: "row",
+    justifyContent: "space-around",
     padding: 10,
   },
 });
