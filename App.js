c299f61ea0f1d43ec41c3d1256a72a155ba5a16c
4SAMU <samuelnzomo4@gmail.com>
2023-03-16 13:32:49 +0300

    Automatically committed changes on my Projects page

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
-import React from 'react'
-import { Pressable, SafeAreaView } from 'react-native/types'
+import React from "react";
+import { Pressable, SafeAreaView } from "react-native/types";
 
 const App = () => {
   function name() {
-    let x=Naame();
-    Alert.alert(x)
-   }
+    let x = Naame();
+    Alert.alert(x);
+  }
 
   return (
     <SafeAreaView>
-<Pressable onPress={onPressFunction}>
-  <Text>I'm pressable!</Text>
-</Pressable>
+      <Pressable onPress={onPressFunction}>
+        <Text>I'm pressable!</Text>
+      </Pressable>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default App
+export default App;
