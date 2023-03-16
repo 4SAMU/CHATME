d880bfc73d7a136c5192c6b610c7503f32bb351e
4SAMU <samuelnzomo4@gmail.com>
2023-03-16 12:36:59 +0300

    Automatically committed changes on my Projects page

--- App.js
+++ App.js
@@ -1,72 +1,41 @@
-import React, {useRef, useState} from 'react';
 import {
-  Button,
-  DrawerLayoutAndroid,
-  Text,
+  ImageBackground,
+  StatusBar,
   StyleSheet,
+  Text,
   View,
-} from 'react-native';
+} from "react-native";
 
-const App = () => {
-  const drawer = useRef(null);
-  const [drawerPosition, setDrawerPosition] = useState('left');
-  const changeDrawerPosition = () => {
-    if (drawerPosition === 'left') {
-      setDrawerPosition('right');
-    } else {
-      setDrawerPosition('left');
-    }
-  };
+const image = { uri: "https://reactjs.org/logo-og.png" };
 
-  const navigationView = () => (
-    <View style={[styles.container, styles.navigationContainer]}>
-      <Text style={styles.paragraph}>I'm in the Drawer!</Text>
-      <Button
-        title="Close drawer"
-        onPress={() => drawer.current.closeDrawer()}
-      />
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={image}
+        resizeMode="cover"
+        style={styles.image}
+      ></ImageBackground>
+      <View style={styles.nav}></View>
+      <StatusBar style="auto" />
     </View>
   );
-
-  return (
-    <DrawerLayoutAndroid
-      ref={drawer}
-      drawerWidth={300}
-      drawerPosition={drawerPosition}
-      renderNavigationView={navigationView}>
-      <View style={styles.container}>
-        <Text style={styles.paragraph}>Drawer on the {drawerPosition}!</Text>
-        <Button
-          title="Change Drawer Position"
-          onPress={() => changeDrawerPosition()}
-        />
-        <Text style={styles.paragraph}>
-          Swipe from the side or press button below to see it!
-        </Text>
-        <Button
-          title="Open drawer"
-          onPress={() => drawer.current.openDrawer()}
-        />
-      </View>
-    </DrawerLayoutAndroid>
-  );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 16,
   },
-  navigationContainer: {
-    backgroundColor: '#ecf0f1',
+  image: {
+    flex: 1,
+    justifyContent: "center",
   },
-  paragraph: {
-    padding: 16,
-    fontSize: 15,
-    textAlign: 'center',
+  text: {
+    color: "white",
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "#000000c0",
   },
 });
-
-export default App;
