003d345760acfbf5e2a91d5c17ebc650f840b2be
4SAMU <samuelnzomo4@gmail.com>
2023-03-16 16:08:56 +0300

    Automatically committed changes on my Projects page

--- components/navbar/styles.js
+++ components/navbar/styles.js
@@ -4,7 +4,7 @@ const navStyles = StyleSheet.create({
   navbar: {
     height: 50,
     backgroundColor: "yellow",
-    justifyContent: "spaceAround",
+    justifyContent: "space-between",
   },
   text: {
     color: "white",
