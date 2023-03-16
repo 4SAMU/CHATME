5ceb51660a091877a24ef115e3df8591a72c11a3
4SAMU <samuelnzomo4@gmail.com>
2023-03-16 13:20:35 +0300

    Automatically committed changes on my Projects page

--- zip.js
+++ zip.js
@@ -2,8 +2,5 @@ function name(params) {
   let A = [{ A: "A" }, { B: "B" }];
   let B = [[{ z: "a" }], [{ y: "y" }]];
 
-  [
-    [{ A: "A" }, [{ z: "a" }]],
-    [{ B: "B" }, [{ y: "y" }]],
-  ];
+  
 }
