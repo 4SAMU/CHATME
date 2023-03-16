2a6ef189c93a745c78f70b0d0c2f26e91b7d7e1a
4SAMU <samuelnzomo4@gmail.com>
2023-03-16 19:58:34 +0300

    Automatically committed changes on my Projects page

--- components/dmchats/DirectMessage.js
+++ components/dmchats/DirectMessage.js
@@ -5,7 +5,7 @@ import dmStyles from "./dmstyles";
 const DirectMessage = () => {
   return (
     <View style={dmStyles.container}>
-      <ScrollView style={[dmStyles.items, { alignItems: "center" }]}>
+      <ScrollView style={[dmStyles.items}>
         <View style={dmStyles.box}>
           <Text style={dmStyles.text}>Mama</Text>
         </View>
