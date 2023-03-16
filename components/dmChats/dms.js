54c13e47f8eb224500317ba884a89133be421296
4SAMU <samuelnzomo4@gmail.com>
2023-03-16 17:40:56 +0300

    Automatically committed changes on my Projects page

--- components/dmChats/dms.js
+++ components/dmChats/dms.js
@@ -5,7 +5,7 @@ import DmStyles from "./styles";
 export const dms = () => {
   return (
     <View style={DmStyles.container}>
-      <View style={}></View>
+      <View style={d}></View>
     </View>
   );
 };
