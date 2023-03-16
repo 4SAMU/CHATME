0c08a80473b5edd6f91c7114a0d9989dc0aa01c5
4SAMU <samuelnzomo4@gmail.com>
2023-03-16 13:34:30 +0300

    Automatically committed changes on my Projects page

--- App.js
+++ App.js
@@ -15,25 +15,7 @@ const App = () => {
     </SafeAreaView>
   );
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      paddingTop: StatusBar.currentHeight,
-      marginHorizontal: 16,
-    },
-    item: {
-      backgroundColor: "#f9c2ff",
-      padding: 20,
-      marginVertical: 8,
-    },
-    header: {
-      fontSize: 32,
-      backgroundColor: "#fff",
-    },
-    title: {
-      fontSize: 24,
-    },
-  });
+  
 };
 
 export default App;
