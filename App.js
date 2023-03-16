7682768fd02d27349b642f850c783f88034ba08d
4SAMU <samuelnzomo4@gmail.com>
2023-03-16 13:59:00 +0300

    Automatically committed changes on my Projects page

--- App.js
+++ App.js
@@ -39,7 +39,7 @@ const styles = StyleSheet.create({
     marginVertical: 8,
   },
   navbar: {
-    width: 100,
+    width: currentHeight,
     height: 100,
     backgroundColor: "blue",
     alignItems: "center",
