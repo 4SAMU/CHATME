cc580a7ccb2659b8b07bcf03df7a8b2d108d20c4
4SAMU <samuelnzomo4@gmail.com>
2023-03-16 17:11:49 +0300

    Automatically committed changes on my Projects page

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -25,7 +25,7 @@ export const Navbar = () => {
       renderNavigationView={() => (
         <View style={navStyles.drawer}>
           <Text>Setting</Text>
-          <Text>Group</Text>
+          <Text>Gr</Text>
           <Text>Log out</Text>
         </View>
       )}
