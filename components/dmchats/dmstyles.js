af8eb4a77dfb16cad2d4dc696d276dddeacb2881
4SAMU <samuelnzomo4@gmail.com>
2023-03-16 19:40:00 +0300

    Automatically committed changes on my Projects page

--- components/dmchats/dmstyles.js
+++ components/dmchats/dmstyles.js
@@ -19,8 +19,7 @@ const dmStyles = StyleSheet.create({
     height: 50,
     backgroundColor: "red",
     padding: 10,
-    justifyContent:"center"
-    border
+    justifyContent: "center",
   },
 });
 
