7f14b7f4b3e4137b6f7cffdf05dcda0541124f91
4SAMU <samuelnzomo4@gmail.com>
2023-03-16 13:31:24 +0300

    Automatically committed changes on my Projects page

--- zip.js
+++ zip.js
@@ -12,3 +12,25 @@ function name() {
     let x=Naame();
     Alert.alert(x)
    }
+
+
+   const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      paddingTop: StatusBar.currentHeight,
+      marginHorizontal: 16,
+    },
+    item: {
+      backgroundColor: "#f9c2ff",
+      padding: 20,
+      marginVertical: 8,
+    },
+    header: {
+      fontSize: 32,
+      backgroundColor: "#fff",
+    },
+    title: {
+      fontSize: 24,
+    },
+  });
+  
