d989d1044dfdfbc5cecacc38729bef215a3d7821
4SAMU <samuelnzomo4@gmail.com>
2023-03-16 16:02:34 +0300

    Automatically committed changes on my Projects page

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Dimensions, Text, View } from "react-native";
+import { Dimensions, StatusBar, Text, View } from "react-native";
 import navStyles from "./styles";
 const { width } = Dimensions.get("window");
 
