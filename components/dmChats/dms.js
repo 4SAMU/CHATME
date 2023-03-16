1a1b2657221cad791701a02c80b1122bcc783e32
4SAMU <samuelnzomo4@gmail.com>
2023-03-16 17:39:35 +0300

    Automatically committed changes on my Projects page

--- components/dmChats/dms.js
+++ components/dmChats/dms.js
@@ -2,8 +2,9 @@ import React from "react";
 import { View } from "react-native";
 
 export const dms = () => {
-  return <View>
-    <View></View>
-
-  </View>;
+  return (
+    <View>
+      <View></View>
+    </View>
+  );
 };
