5cd22a13a44e753911adc9d1084482e281dd1382
4SAMU <samuelnzomo4@gmail.com>
2023-03-16 13:26:18 +0300

    Automatically committed changes on my Projects page

--- App.js
+++ App.js
@@ -8,24 +8,7 @@ import {
   StatusBar,
 } from "react-native";
 
-const DATA = [
-  {
-    title: "Main dishes",
-    data: ["Pizza", "Burger", "Risotto"],
-  },
-  {
-    title: "Sides",
-    data: ["French Fries", "Onion Rings", "Fried Shrimps"],
-  },
-  {
-    title: "Drinks",
-    data: ["Water", "Coke", "Beer"],
-  },
-  {
-    title: "Desserts",
-    data: ["Cheese Cake", "Ice Cream"],
-  },
-];
+
 
 export default App = () => (
   <SafeAreaView style={styles.container}>
