70a86a5ce444c25b1fd844ff7ad3b7017580623d
4SAMU <samuelnzomo4@gmail.com>
2023-03-16 18:55:47 +0300

    Automatically committed changes on my Projects page

--- components/dmchats/dmstyles.js
+++ components/dmchats/dmstyles.js
@@ -5,7 +5,7 @@ const dmStyles = StyleSheet.create({
     backgroundColor: "red",
     marginTop: 50,
     width: "100%",
-    height: "93%",
+    height: "92%",
     marginLeft: 0,
   },
   items: {
