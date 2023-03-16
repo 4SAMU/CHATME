32cd1d6b0ecfbfe76a4b8a4604d6cd95b0d49f51
4SAMU <samuelnzomo4@gmail.com>
2023-03-16 17:22:21 +0300

    Automatically committed changes on my Projects page

---
+++ components/navbar/navbar.js
@@ -0,0 +1,53 @@
+import React, { useRef } from "react";
+import {
+  Dimensions,
+  Text,
+  TouchableOpacity,
+  View,
+  DrawerLayoutAndroid,
+} from "react-native";
+import navStyles from "./styles";
+import Icon from "react-native-vector-icons/Feather";
+const { width } = Dimensions.get("window");
+
+export const Navbar = () => {
+  const drawerRef = useRef(null);
+
+  const openDrawer = () => {
+    drawerRef.current.openDrawer();
+  };
+
+  return (
+    <DrawerLayoutAndroid
+      ref={drawerRef}
+      drawerWidth={250}
+      drawerPosition="right"
+      renderNavigationView={() => (
+        <View style={navStyles.drawer}>
+          <Text style={navStyles.drawerText}>
+            <Icon name="settings" size={25} color="#fff" /> Setting
+          </Text>
+          <Text style={navStyles.drawerText}>
+            <Icon name="users" size={25} color="#fff" /> Group
+          </Text>
+          <Text style={navStyles.drawerText}>
+            <Icon name="message-circle" size={25} color="#fff" /> DMS
+          </Text>
+          <Text style={navStyles.drawerText}>Bot</Text>
+          <Text style={navStyles.drawerText}>
+            <Icon name="log-out" size={25} color="#fff" /> Log out
+          </Text>
+        </View>
+      )}
+    >
+      <View style={[navStyles.navbar, { width }]}>
+        <Text style={navStyles.navText}>ChatMe</Text>
+        <TouchableOpacity onPress={openDrawer}>
+          <View>
+            <Icon name="menu" size={25} color="#fff" />
+          </View>
+        </TouchableOpacity>
+      </View>
+    </DrawerLayoutAndroid>
+  );
+};
