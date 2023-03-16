a3ae70f6020527e39df06c4b7e900d9ef5dbb3bc
4SAMU <samuelnzomo4@gmail.com>
2023-03-16 18:08:23 +0300

    Automatically committed changes on my Projects page

--- components/navbar/styles.js
+++ components/navbar/styles.js
@@ -2,7 +2,7 @@ import { StyleSheet } from "react-native";
 
 const navStyles = StyleSheet.create({
   navbar: {
-    position:''
+    position:'absolute'
     height: 50,
     backgroundColor: "#FFBB36",
     flexDirection: "row",
