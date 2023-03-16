7cc2dccf54e21769cc0e44bb869bb0ebd2e5d51e
4SAMU <samuelnzomo4@gmail.com>
2023-03-16 13:48:45 +0300

    Automatically committed changes on my Projects page

--- App.js
+++ App.js
@@ -18,7 +18,7 @@ const App = () => {
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style></View>
+      <View style=></View>
       <Pressable onPress={onPressFunction}>
         <Text>I'm pressable!</Text>
       </Pressable>
