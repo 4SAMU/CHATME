2a989acda39b0dc135cd4a777302e3feebd55c99
4SAMU <samuelnzomo4@gmail.com>
2023-03-16 16:28:03 +0300

    Automatically committed changes on my Projects page

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -6,7 +6,7 @@ const { width } = Dimensions.get("window");
 export const Navbar = () => {
   return (
     <View style={[navStyles.navbar, { width }]}>
-      <Text styles={navStyles.navText}>ChatMe</Text>
+      <Text styles={navStyles.}>ChatMe</Text>
       <Text styles={navStyles.navText}>ChatMe</Text>
       <Text styles={navStyles.navText}>ChatMe</Text>
       <Text styles={navStyles.navText}>ChatMe</Text>
