08aed743c11262d83b2f55b48a624237c280d332
4SAMU <samuelnzomo4@gmail.com>
2023-03-16 17:45:28 +0300

    Automatically committed changes on my Projects page

--- components/dmChats/dms.js
+++ components/dmChats/dms.js
@@ -2,7 +2,7 @@ import React from "react";
 import { View } from "react-native";
 import DmStyles from "./styles";
 
-export const DMs = () => {
+export const Dms = () => {
   return (
     <ScrollView style={DmStyles.container}>
       <View style={DmStyles.box}>mama</View>
