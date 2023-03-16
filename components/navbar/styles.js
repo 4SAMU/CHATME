10fa3be82a3eeac71c3ca5ba847ebf1b936d8d5b
4SAMU <samuelnzomo4@gmail.com>
2023-03-16 16:32:01 +0300

    Automatically committed changes on my Projects page

--- components/navbar/styles.js
+++ components/navbar/styles.js
@@ -10,16 +10,9 @@ const navStyles = StyleSheet.create({
     padding: 10,
   },
   navText: {
-    marginTop: 16,
-    paddingVertical: 8,
-    borderWidth: 4,
-    borderColor: '#20232a',
-    borderRadius: 6,
-    backgroundColor: '#61dafb',
-    color: '#20232a',
-    textAlign: 'center',
-    fontSize: 30,
-    fontWeight: 'bold',
+    color: "#20232a",
+    fontSize: 20,
+    fontWeight: "bold",
   },
 });
 
