d4c62edb17eaef9526a5f348ee7ef00e6f2d5e77
4SAMU <samuelnzomo4@gmail.com>
2023-03-16 16:04:09 +0300

    Automatically committed changes on my Projects page

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { Navbar } from "./components/navbar/navbar";
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <StatusBar barStyle="default" backgroundColor="red" />
+      <StatusBar barStyle="default" backgroundColor="" />
       <Navbar />
     </SafeAreaView>
   );
