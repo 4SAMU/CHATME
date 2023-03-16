717754612c30d5f2337cac406c84e327254b1c73
4SAMU <samuelnzomo4@gmail.com>
2023-03-16 13:28:56 +0300

    Automatically committed changes on my Projects page

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default App = () => (
 
 
   <SafeAreaView style={styles.container}>
-    <Pressable onPress={()={const data=Naame}}>
+    <Pressable onPress={()={const data=Naame()}}>
       <Text>I'm pressable!</Text>
     </Pressable>
   </SafeAreaView>
