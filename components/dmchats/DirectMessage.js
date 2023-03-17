b2ae0c9d1c1b72466fb60399cfd5941e02aa2c01
4SAMU <samuelnzomo4@gmail.com>
2023-03-17 11:18:19 +0300

    Automatically committed changes on my Projects page

--- components/dmchats/DirectMessage.js
+++ components/dmchats/DirectMessage.js
@@ -45,7 +45,7 @@ const DirectMessage = () => {
           />
           <Text style={dmStyles.text}>Mjukuu</Text>
         </View>
-        <Text style={dmStyles.footer}>end to end</Text>
+        <Text style={dmStyles.footer}></Text>
       </ScrollView>
     </View>
   );
